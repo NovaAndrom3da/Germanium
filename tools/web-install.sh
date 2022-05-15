8adc16153ec860fc30bb15963a51f1713e2e6a2e
Froggo <92762044+Froggo8311@users.noreply.github.com>
2022-05-15 02:14:14 +0000

    Fix remote install script

--- tools/web-install.sh
+++ tools/web-install.sh
@@ -1,4 +1,9 @@
-python3 -m pip install distro nuitka
-git clone https://github.com/Froggo8311/Germanium.git
-cd Germanium
-python3 tools/install_src.py
+echo "Installing required packages...";
+python3 -m pip install distro nuitka;
+
+echo "Downloading Germanium...";
+git clone https://github.com/Froggo8311/Germanium.git;
+
+echo "Running install script...";
+cd Germanium;
+python3 tools/install_src.py;
