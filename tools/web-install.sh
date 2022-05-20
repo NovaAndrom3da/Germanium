4066aa72a9ed327d554e652db52df5034d697bd6
DannyVn2000 <77970269+NLTD2010@users.noreply.github.com>
2022-05-20 07:20:05 +0700

    Update web-install.sh

--- tools/web-install.sh
+++ tools/web-install.sh
@@ -3,10 +3,9 @@ python3 -m pip install --quiet distro nuitka > germanium_pip_install.log
 rm germanium_pip_install.log
 
 echo "Downloading Germanium..."
-touch Germanium
-cd Germanium
 git clone --quiet https://github.com/Froggo8311/Germanium.git > germanium_git_clone.log
 rm germanium_git_clone.log
 
 echo "Running install script..."
+cd Germanium
 python3 tools/install.py
